a9e829a12b4ef566a1f30cfae18ea549f5fadadb
Jerel Navarrete <navarrete.jerel@gmail.com>
2014-09-16 12:43:49 -0700

    fixed else if bug

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(num)
-  if i % 15 == 0
+  if num % 15 == 0
     puts "FizzBuzz"
-  else if num % 5 == 0
+  elsif num % 5 == 0
     puts "Buzz"
-  else if num % 3 == 0
+  elsif num % 3 == 0
     puts "Fizz"
   else 
     puts num
