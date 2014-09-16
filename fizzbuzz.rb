b1af526e40adbc46bace60154e69348bd3c9e0de
Jerel Navarrete <navarrete.jerel@gmail.com>
2014-09-16 11:08:41 -0700

    updating intents

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,12 @@
-class Fizzbuzz
-    def fizz(i)
-        if i % 15 == 0
-            puts "FizzBuzz"
-        else if i % 5 == 0
-            puts "Buzz"
-        else if 1 % 3 == 0
-            puts "Fizz"
-        else 
-            puts i
-        end
-    end
+def fizzbuzz(num)
+  if i % 15 == 0
+    puts "FizzBuzz"
+  else if num % 5 == 0
+    puts "Buzz"
+  else if num % 3 == 0
+    puts "Fizz"
+  else 
+    puts num
+  end
 end
+
